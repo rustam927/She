7670232a812bf9b03a469d86e515fb956a2b5e34
rustam927 <rustamrahmonov927@gmail.com>
2025-07-11 08:12:33 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -7,15 +7,21 @@ let na = document.querySelector(".na")
 
 div3.onclick = function() {
 	div3.style.top = 400 + "px"
+	div3.style.backgroundImage = "url(1.jpeg)"
+	div3.style.backgroundSize = "cover"
 }
 
 div2.onclick = function() {
 	div2.style.left = 650 + "px"
+	div2.style.backgroundImage = "url(2.jpeg)"
+	div2.style.backgroundSize = "cover"
 }
 
 div1.onclick = function() {
 	btn.style.top = 500 + "px"
 	btn.style.left = 725 + "px"
+	div1.style.backgroundImage = "url(3.jpeg)"
+	div1.style.backgroundSize = "cover"
 }
 
 btn.onclick = function() {
